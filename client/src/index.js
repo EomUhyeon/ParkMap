66141d0dae12246324be3bfc80d5658bb21c7b55
Eom <righthyeon00@gmail.com>
2024-08-29 23:40:27 +0900

    start project

---
+++ client/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.js";
+
+const root_id = document.getElementById("root");
+const root = ReactDOM.createRoot(root_id);
+root.render(<App />);
