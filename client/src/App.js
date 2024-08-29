8e0f9679c8c03556885dc6ca143db6f4befb0df0
Eom <righthyeon00@gmail.com>
2024-08-29 23:40:08 +0900

    start project

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import MapUploader from "./map.js";
+import Menu from "./menu.js";
+
+function App() {
+
+  return (
+    <div className="App">
+        <MapUploader />
+        <Menu />
+    </div>
+  );
+};
+
+
+export default App;
