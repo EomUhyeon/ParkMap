bb9eb7e9ed63fae5aa930e17b0fdc7e01add4f25
Eom <righthyeon00@gmail.com>
2024-09-01 02:04:16 +0900

    팝업 데이터 표시

--- client/src/map.js
+++ client/src/map.js
@@ -22,7 +22,7 @@ function MapUploader() {
         );
     };
 
-    const getClassByCategory = (category) => {
+    const parkCategory = (category) => {
         switch (category) {
             case '어린이공원': return 'marker-children-park';
             case '근린공원': return 'marker-neighborhood-park';
@@ -42,7 +42,7 @@ function MapUploader() {
                 {parkData.map((location) => (
                     <Marker
                         icon={new L.DivIcon({
-                            className: `custom-marker ${getClassByCategory(location.공원구분)}`,
+                            className: `custom-marker ${parkCategory(location.공원구분)}`,
                             iconSize: [20, 20],         // 크기
                             iconAnchor: [10, 10],       // 원의 중심 설정
                             popupAnchor: [0, -10],      // 팝업 위치
@@ -55,12 +55,19 @@ function MapUploader() {
                             {location.공원명}
                         </Tooltip> */}
                         <Popup>
-                            {location.공원명}<br />
-                            분류: {location.공원구분}<br />
-                            면적: {location.공원면적}㎡<br />
-                            시설: {location.공원보유시설 ? location.공원보유시설 : '없음'}<br />
-                            관리기관: {location.관리기관명}<br />
-                            전화번호: {location.전화번호}
+                            {location.공원명 && <><strong>{location.공원명}</strong><br /></>}
+                            {location.공원구분 && <>분류: {location.공원구분}<br /></>}
+                            {location.소재지지번주소 && <>지번 주소: {location.소재지지번주소}<br /></>}
+                            {location.소재지도로명주소 && <>도로명 주소: {location.소재지도로명주소}<br /></>}
+                            {location.공원면적 && <>면적: {location.공원면적}㎡<br /></>}
+                            {location['공원보유시설(운동시설)'] && <>운동시설: {location['공원보유시설(운동시설)']}<br /></>}
+                            {location['공원보유시설(유희시설)'] && <>유희시설: {location['공원보유시설(유희시설)']}<br /></>}
+                            {location['공원보유시설(편익시설)'] && <>편익시설: {location['공원보유시설(편익시설)']}<br /></>}
+                            {location['공원보유시설(교양시설)'] && <>교양시설: {location['공원보유시설(교양시설)']}<br /></>}
+                            {location['공원보유시설(기타시설)'] && <>기타시설: {location['공원보유시설(기타시설)']}<br /></>}
+                            {location.관리기관명 && <>관리기관: {location.관리기관명}<br /></>}
+                            {location.전화번호 && <>관리기관 번호: {location.전화번호}<br /></>}
+                            {location.데이터기준일자 && <>정보기준일자: {location.데이터기준일자}<br /></>}
                         </Popup>
                     </Marker>
                 ))}
