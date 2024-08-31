dbdec545db05ec5250de61f697f752da6f82c09f
Eom <righthyeon00@gmail.com>
2024-09-01 07:58:55 +0900

    시설 검색 기능 추가

--- client/src/menu.js
+++ client/src/menu.js
@@ -5,26 +5,75 @@ import "./menu.css";
 function Menu() {
     const LeftMenu = () => {
         const [menuOpen, setMenuOpen] = useState(true);
-        const left_menu_btn = () => {
+        const left_menu = menuOpen ? 'left_menu' : 'left_menu left_menu_closed';
+
+        const LeftMenuBnt = () => {
             setMenuOpen(!menuOpen);
         };
-        const left_menu = menuOpen ? 'left_menu' : 'left_menu left_menu_closed';
 
         const Search = () => {
             return(
-                <div className="search_box">
-                <input type="text" placeholder="공원 검색..." className="search_bar" />
-                <button className="search_bnt"></button>
-                </div>
+                <>
+                    <div className="search_box">
+                        <input type="text" placeholder="공원 검색..." className="search_bar" />
+                        <button className="search_bnt"></button>
+                    </div>
+                </>
             );  
+        };
+
+        const Facilities = () => {
+            return(
+                <div className="facilities_box">
+                    <button className="facilities_bnt">🚻</button>
+                    <button className="facilities_bnt">🅿️</button>
+                    <button className="facilities_bnt">🎠</button>
+                    <button className="facilities_bnt">🏟️</button>
+                    <button className="facilities_bnt">🏀</button>
+                    <button className="facilities_bnt">🏸</button>
+                    <p className="facilities_text">화장실</p>
+                    <p className="facilities_text">주차장</p>
+                    <p className="facilities_text">놀이터</p>
+                    <p className="facilities_text">운동장</p>
+                    <p className="facilities_text">농구장</p>
+                    <p className="facilities_text">배트민턴장</p>
+                    <button className="facilities_bnt">🏋️‍♂️</button>
+                    <button className="facilities_bnt"></button>
+                    <button className="facilities_bnt"></button>
+                    <button className="facilities_bnt"></button>
+                    <button className="facilities_bnt"></button>
+                    <button className="facilities_bnt"></button>
+                    <p className="facilities_text">운동기구</p>
+                    <p className="facilities_text">주차장</p>
+                    <p className="facilities_text">놀이터</p>
+                    <p className="facilities_text">운동장</p>
+                    <p className="facilities_text">농구장</p>
+                    <p className="facilities_text">배트민턴장</p>
+                </div>
+            );
+        };
+
+        const SearchContents = () => {
+            return(
+                <div>검색 내용
+
+                </div>
+            );
         };
 
         return (
             <div className={left_menu}>
-                <button className="left_menu_btn" onClick={left_menu_btn}>
+                <button className="left_menu_btn" onClick={LeftMenuBnt}>
                 {menuOpen ? "<" : ">"}
                 </button>
                 <Search />
+                <Facilities />
+                <hr className="gray-line"></hr>
+                <SearchContents />
+                <hr className="gray-line"></hr>
+                <div>추천 공원</div>
+                <hr className="gray-line"></hr>
+                <div>즐겨찾기</div>
             </div>
         );
     };
